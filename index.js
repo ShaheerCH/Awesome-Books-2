b8d228017aad62a3b7ce15c10c17fddca0ebbc42
shaheer ch <shaheer.chry@gmail.com>
2022-06-10 10:04:11 +0500

    Fix Linters

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-import displayList from "./modules/List.js";
-import showTime from "./modules/Time.js";
-import add from "./modules/Add.js";
-import displayContact from "./modules/Contact.js";
+import displayList from './modules/List.js';
+import showTime from './modules/Time.js';
+import add from './modules/Add.js';
+import displayContact from './modules/Contact.js';
 
 showTime();
 displayList();
 const reset = () => {
-  document.getElementById("content").innerHTML = "";
+  document.getElementById('content').innerHTML = '';
 };
 
-document.getElementById("list").addEventListener("click", (event) => {
+document.getElementById('list').addEventListener('click', (event) => {
   reset();
   displayList();
   event.stopPropagation();
 });
 
-document.getElementById("add").addEventListener("click", (event) => {
+document.getElementById('add').addEventListener('click', (event) => {
   reset();
   add();
   event.stopPropagation();
 });
 
-document.getElementById("contact-us").addEventListener("click", (event) => {
+document.getElementById('contact-us').addEventListener('click', (event) => {
   reset();
   displayContact();
   event.stopPropagation();
